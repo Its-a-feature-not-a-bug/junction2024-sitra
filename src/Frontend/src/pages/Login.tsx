2623bdf1658dce611ef7f36fbd53f4095cc9e3b0
hann-mbp14 <gwu.suomi@outlook.com>
2024-11-10 00:17:31 +0200

    try to fix deploy issue

--- src/Frontend/src/pages/Login.tsx
+++ src/Frontend/src/pages/Login.tsx
@@ -1,47 +1,48 @@
-import React, { useRef, useState } from 'react';
-import { useAuth } from '../AuthContext';
-import { useNavigate } from 'react-router-dom';
-import api from '../axiosConfig';
-import ReCAPTCHA from 'react-google-recaptcha'
+import React, { useRef, useState } from "react";
+import { useAuth } from "../AuthContext";
+import { useNavigate } from "react-router-dom";
+import api from "../axiosConfig";
+import ReCAPTCHA from "react-google-recaptcha";
 
 const Login: React.FC = () => {
-  const recaptcha = useRef<ReCAPTCHA | null>(null)
-  const [nickname, setNickname] = useState<string>('');
+  const recaptcha = useRef<ReCAPTCHA | null>(null);
+  const [nickname, setNickname] = useState<string>("");
   const { setToken } = useAuth();
   const navigate = useNavigate();
 
   const handleLogin = async () => {
-    const captchaValue = recaptcha.current?.getValue()
+    const captchaValue = recaptcha.current?.getValue();
     if (!captchaValue) {
-      alert('Please verify the reCAPTCHA!')
+      alert("Please verify the reCAPTCHA!");
       return;
     } else {
-      const res = await api.post('/verify', {
+      const res = await api.post("/verify", {
         captchaValue: captchaValue,
       });
-    
+
       if (res.data.success) {
         // make form submission
-        alert('Form submission successful!')
+        alert("Form submission successful!");
       } else {
-        alert('reCAPTCHA validation failed!')
+        alert("reCAPTCHA validation failed!");
       }
     }
 
-
-
     if (!nickname.trim()) {
-      alert('Nickname is required!');
+      alert("Nickname is required!");
       return;
     }
     try {
-      const response = await axios.post(`${import.meta.env.VITE_API_URL}/login/anonymous`, { nickname });
+      const response = await api.post(
+        `${import.meta.env.VITE_API_URL}/login/anonymous`,
+        { nickname }
+      );
       // const response = await axios.post('http://localhost:8000/login/anonymous', { nickname });
       const accessToken = response.data.access_token;
       setToken(accessToken); // Store the token in AuthContext
-      navigate('/'); // Redirect to the main page
+      navigate("/"); // Redirect to the main page
     } catch (error) {
-      console.error('Login failed:', error);
+      console.error("Login failed:", error);
     }
   };
 
@@ -57,7 +58,10 @@ const Login: React.FC = () => {
         />
       </label>
       <button onClick={handleLogin}>Login</button>
-      <ReCAPTCHA ref={recaptcha} sitekey={import.meta.env.VITE_RECAPTCHA_SITE_KEY} />
+      <ReCAPTCHA
+        ref={recaptcha}
+        sitekey={import.meta.env.VITE_RECAPTCHA_SITE_KEY}
+      />
     </div>
   );
 };

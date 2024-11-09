6fbb78ffc22f3d640d497b35b01bddcc7ffb579c
hann-mbp14 <gwu.suomi@outlook.com>
2024-11-09 20:29:03 +0200

    comment unused react

--- src/Frontend/src/components/Logout/Logout.tsx
+++ src/Frontend/src/components/Logout/Logout.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useAuth } from '../../AuthContext';
-import { Button } from '@mui/material';
+// import React from 'react';
+import { useAuth } from "../../AuthContext";
+import { Button } from "@mui/material";
 
 const Logout = () => {
   const { logout } = useAuth();

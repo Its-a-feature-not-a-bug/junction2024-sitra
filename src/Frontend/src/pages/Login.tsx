7fdf61e48eb9b4a6eb6f6f351c6451e7a7d0502e
hann-mbp14 <gwu.suomi@outlook.com>
2024-11-10 05:24:40 +0200

    added icon anonymous

--- src/Frontend/src/pages/Login.tsx
+++ src/Frontend/src/pages/Login.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { useAuth } from '../AuthContext';
-import { useNavigate } from 'react-router-dom';
-import api from '../axiosConfig';
-import ReCAPTCHA from 'react-google-recaptcha';
+import React, { useEffect, useRef, useState } from "react";
+import { useAuth } from "../AuthContext";
+import { useNavigate } from "react-router-dom";
+import api from "../axiosConfig";
+import ReCAPTCHA from "react-google-recaptcha";
 import {
   Box,
   Button,
@@ -10,52 +10,52 @@ import {
   TextField,
   Typography,
   Paper,
-} from '@mui/material';
+} from "@mui/material";
 
 const Login: React.FC = () => {
   const recaptcha = useRef<ReCAPTCHA | null>(null);
-  const [nickname, setNickname] = useState<string>('');
+  const [nickname, setNickname] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
   const { setToken, token } = useAuth();
   const navigate = useNavigate();
 
   useEffect(() => {
     if (token) {
-      navigate('/');
+      navigate("/");
     }
   }, [token, navigate]);
 
   const handleLogin = async () => {
     const captchaValue = recaptcha.current?.getValue();
     if (!captchaValue) {
-      alert('Please verify the reCAPTCHA!');
+      alert("Please verify the reCAPTCHA!");
       return;
     }
 
     if (!nickname.trim()) {
-      alert('Nickname is required!');
+      alert("Nickname is required!");
       return;
     }
 
     setLoading(true);
     try {
-      const verifyRes = await api.post('/verify', {
+      const verifyRes = await api.post("/verify", {
         captchaValue,
       });
 
       if (verifyRes.data.success) {
-        const response = await api.post('/login/anonymous', {
+        const response = await api.post("/login/anonymous", {
           nickname,
         });
         const accessToken = response.data.access_token;
         setToken(accessToken);
-        navigate('/');
+        navigate("/");
       } else {
-        alert('reCAPTCHA validation failed!');
+        alert("reCAPTCHA validation failed!");
       }
     } catch (error) {
-      console.error('Login failed:', error);
-      alert('An error occurred during login.');
+      console.error("Login failed:", error);
+      alert("An error occurred during login.");
     }
     setLoading(false);
   };
@@ -63,20 +63,37 @@ const Login: React.FC = () => {
   return (
     <Container maxWidth="sm">
       <Paper elevation={3} sx={{ padding: 4, marginTop: 8 }}>
-        <Typography variant="h4" component="h1" align="center" gutterBottom>
+        <Box
+          component="img"
+          src="/img/anonymity.png"
+          alt="Anonymity Icon"
+          sx={{
+            width: 100,
+            height: 100,
+            display: "block",
+            margin: "0 auto 2px auto",
+          }}
+        />
+        <Typography
+          variant="h4"
+          component="h1"
+          align="center"
+          gutterBottom
+          sx={{ fontWeight: 500 }}
+        >
           Anonymous Login
         </Typography>
-        <Typography align="center">
+        {/* <Typography align="center">
           Anonymous login reduces entry barriers, enhancing user engagement and
           privacy.
-        </Typography>
+        </Typography> */}
         <Box
           component="form"
           sx={{
-            display: 'flex',
-            flexDirection: 'column',
+            display: "flex",
+            flexDirection: "column",
             gap: 2,
-            marginTop: 2,
+            marginTop: 4,
           }}
           onSubmit={(e) => {
             e.preventDefault();

7dba19b66b4d5c61c71313bbb6863b9d02c72c17
hann-mbp14 <gwu.suomi@outlook.com>
2024-11-10 06:36:12 +0200

    try fx cros

--- src/Backend/main.py
+++ src/Backend/main.py
@@ -9,31 +9,34 @@ metadata.create_all(bind=engine)
 
 app = FastAPI()
 
- 
-
+# Define allowed origins
 origins = [
+    "*",
     "http://localhost:5173",
-    "https://junction2024-sitra-frontend.onrender.com"
+    "https://junction2024-sitra-frontend.onrender.com",
 ]
 
+# Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  # Allow specified origins
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
 )
+
 
 @app.on_event("startup")
 async def startup():
     await database.connect()
 
+
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
+
 
 # Include routers with /api/ prefix
 app.include_router(auth.router, prefix="/api")
 app.include_router(conversations.router, prefix="/api")
 app.include_router(captcha.router, prefix="/api")
-

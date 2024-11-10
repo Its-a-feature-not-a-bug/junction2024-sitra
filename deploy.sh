a1a23def3a41946241a3763cd5bcec622d5cdf62
noahwo <26848129+noahwo@users.noreply.github.com>
2024-11-10 04:30:15 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # Deploy backend service
 curl --request POST \
-    --url "https://api.render.com/v1/services/$BACKEND_SERVICE_ID/deploys" \
+    --url "https://api.render.com/v1/services/$BACKEND_SRV/deploys" \
     --header 'accept: application/json' \
     --header "authorization: Bearer $RENDER_API_KEY" \
     --header 'content-type: application/json' \
@@ -12,7 +12,7 @@ echo "Backend service deployment triggered"
 
 # Deploy frontend service
 curl --request POST \
-    --url "https://api.render.com/v1/services/$FRONTEND_SERVICE_ID/deploys" \
+    --url "https://api.render.com/v1/services/$FRONTEND_SRV/deploys" \
     --header 'accept: application/json' \
     --header "authorization: Bearer $RENDER_API_KEY" \
     --header 'content-type: application/json' \
